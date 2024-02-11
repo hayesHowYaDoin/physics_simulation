fac31cb1e0e39e3b41ab981436894a9d82753c40
Jordan Hayes <jordanhayes98@gmail.com>
2024-02-11 10:29:12 -0500

    Corrected error in simulation_window.cpp caused by overwritten save

--- src/simulation_window.cpp
+++ src/simulation_window.cpp
@@ -29,8 +29,8 @@ void SimulationWindow::paintEvent(QPaintEvent *event)
     painter.setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::RoundCap));
     painter.setBrush(QBrush(Qt::green, Qt::SolidPattern));
 
-    for(auto const& point : m_points)
+    for (const auto &point : m_points)
     {
-        painter.drawEllipse(point, 5, 5); // center, width, height
+        painter.drawEllipse(point, 50, 50); // center, width, height
     }
 }
